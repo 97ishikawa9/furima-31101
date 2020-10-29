1c5c18d3ed93239a7b36bf4eba0e518af0a3492a
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-10-29 19:29:34 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :name, presence: true
+  validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true

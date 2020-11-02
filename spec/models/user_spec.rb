ef1bef16358c0f3c4ee38cd963025b211b2e2fb2
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-02 19:27:19 +0900

    メールアドレスは@を含む必要があるテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
+      it 'emailに@がない場合登録できない' do
+        @user.email = 'mail.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?

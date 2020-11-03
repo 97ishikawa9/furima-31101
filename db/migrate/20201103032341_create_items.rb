cc378b04523fbffe5fd1561001e8d03df7b6e3bb
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-03 16:26:43 +0900

    カラムオプション追加

--- db/migrate/20201103032341_create_items.rb
+++ db/migrate/20201103032341_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.string :category_id
-      t.string :status_id
-      t.string :send_price_id
-      t.string :prefectures_id
-      t.string :sending_days_id
-      t.string :price
+      t.string :name, null: false
+      t.text :description, null: false
+      t.string :category_id, null: false
+      t.string :status_id, null: false
+      t.string :send_price_id, null: false
+      t.string :prefectures_id, null: false
+      t.string :sending_days_id, null: false
+      t.string :price, null: false
       t.references :user
       t.timestamps
     end

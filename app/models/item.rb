b4e33bc1da7715ca7e670d9fe33e671369e3db63
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-03 15:27:52 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :send_price_id, presence: true
+  validates :prefectures_id, presence: true
+  validates :sending_days_id, presence: true
+  validates :price, presence: true
 end

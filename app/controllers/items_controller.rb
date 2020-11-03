352f58317db7ad1561462a0134778933d9c0c521
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-03 15:20:46 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
   end
 
-  def new
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
   end
-  
+
+  private
+
+  def message_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :send_price_id, :prefectures_id, :sending_days_id, :price).merge(user_id: current_user.id)
+  end
 end

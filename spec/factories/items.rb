c9a3f80c96a7a918b9bf72e9a94e22275c5fa2f9
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-04 18:01:23 +0900

    FactoryBotの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    description {Faker::Lorem.sentence}
+    category_id { 2 }
+    status_id { 2 }
+    send_price_id { 2 }
+    prefectures_id { 2 }
+    sending_days_id { 2 }
+    price { 2000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end

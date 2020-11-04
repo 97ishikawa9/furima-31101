aed7ffeb5e29ae8e8674de8a8d51d69c2b0cd916
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-04 15:52:04 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name,:description,:category,:send_price,:prefectures,:sending_days,:price, presence: true
-  validates :category_id,:status_id,:send_price_id,:prefectures_id,:sending_days_id, numericality: { other_than: 1 }
+  validates :name, :image, :description, :category_id, :send_price_id, :prefectures_id, :sending_days_id, presence: true
+  validates :category_id, :status_id, :send_price_id, :prefectures_id, :sending_days_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end

4667394279588f2fb653c2ace4677fe3ff9e1b93
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-11-13 17:08:38 +0900

    Formオブジェクトの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_num, :prefectures_id, :city, :address_num, :building_name, :phone_num, :token
+  attr_accessor :post_num, :prefectures_id, :city, :address_num, :building_name, :phone_num, :token, :user_id, :item_id, :purchase_id
 
   with_options presence: true do
     validates :post_num, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -13,10 +13,7 @@ class PurchaseAddress
   validates :prefectures_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-    user = User.create(nickname: nickname, email: email, family_name: family_name, first_name: first_name, family_name_kana: family_name_kana, first_name_kana: first_name_kana, birthday: birthday )
-    item = Item.create(name: name, description: description, category_id: category_id, status_id: status_id, send_price_id: send_price_id, prefectures_id: prefectures_id, price: price, user_id: user.id)
-    Purchase.create(user_id: user.id, item_id: item.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(post_num: post_num, prefectures_id: prefectures_id, city: city, address_num: address_num, building_name: building_name, phone_num: phone_num, purchase_id: purchase.id)
   end
-
 end

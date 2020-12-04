cea41ba88e6fb8d96b1e21be44bcd2e5493a05e1
97ishikawa9 <ishikawaidolbaseballradiosan@gmail.com>
2020-12-04 15:07:31 +0900

    モデルへの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,4 +27,12 @@ class Item < ApplicationRecord
     validates :sending_days_id
   end
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+
+  def self.search(search)
+    if search != ""
+      Item.where('text LIKE(?)', "%#{search}%")
+    else
+      Item.all
+    end
+  end
 end
